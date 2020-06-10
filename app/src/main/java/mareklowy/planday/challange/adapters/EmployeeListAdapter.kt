ce2c4f74f36f5708d9183b9257955bfbb12ae064
Marek Lowy <mareklowy@protonmail.ch>
2020-06-10 17:54:46 +0200

    PD: Custom adapter to display employees implemented

---
+++ app/src/main/java/mareklowy/planday/challange/adapters/EmployeeListAdapter.kt
@@ -0,0 +1,54 @@
+package mareklowy.planday.challange.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.cell_employee.view.*
+import mareklowy.planday.challange.R
+import mareklowy.planday.challange.api.data.EmployeeData
+import org.jetbrains.anko.sdk25.coroutines.onClick
+
+class EmployeeListAdapter(
+    private val context: Context?,
+    var employees: List<EmployeeData>,
+    var onClick: (employee: EmployeeData) -> Unit
+) : RecyclerView.Adapter<EmployeeListAdapter.EmployeeViewHolder>() {
+
+    override fun getItemCount() = employees.size
+
+    override fun onBindViewHolder(holder: EmployeeViewHolder, position: Int) {
+        employees[position].let {
+            holder.bind(it)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EmployeeViewHolder {
+        val view = LayoutInflater.from(context)
+            .inflate(R.layout.cell_employee, parent, false)
+
+        return EmployeeViewHolder(view)
+    }
+
+    //MARK: ViewHolder
+    inner class EmployeeViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+
+        fun bind(employee: EmployeeData) {
+            view.apply {
+                employee_cell_name_textview.text = "${employee.firstName} ${employee.lastName}"
+
+                Glide.with(context)
+                    .load(R.drawable.dw_profile_placeholder)
+                    .apply(RequestOptions.circleCropTransform())
+                    .into(employee_cell_image_imageview)
+
+                view.onClick {
+                    this@EmployeeListAdapter.onClick
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/mareklowy/planday/challange/fragments/EmployeeListFragment.kt
+++ app/src/main/java/mareklowy/planday/challange/fragments/EmployeeListFragment.kt
@@ -6,8 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_employee_list.*
 import mareklowy.planday.challange.R
+import mareklowy.planday.challange.adapters.EmployeeListAdapter
+import mareklowy.planday.challange.api.ApiResponseType
 import mareklowy.planday.challange.api.DataProvider
+import mareklowy.planday.challange.api.data.EmployeeData
 
 class EmployeeListFragment : Fragment() {
 
@@ -21,9 +26,36 @@ class EmployeeListFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
+        setupRecycler()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        updateUI()
+    }
+
+    private fun updateUI() {
         DataProvider.getEmployees { response, employees ->
             Log.d("EMP", "Employees: ${employees?.size ?: "NULL"}")
+            if (response.type == ApiResponseType.SUCCESS) {
+                employees_list_recyclerview?.apply {
+                    (adapter as EmployeeListAdapter).employees = employees ?: listOf()
+                    (adapter as EmployeeListAdapter).notifyDataSetChanged()
+                }
+            }
         }
     }
 
+    private fun setupRecycler() {
+        employees_list_recyclerview.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = EmployeeListAdapter(context, listOf()) {
+                loadEmployee(it)
+            }
+        }
+    }
+
+    private fun loadEmployee(employee: EmployeeData) {
+        //
+    }
 }
